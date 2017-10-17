48f4cf141f9018a96365fc6d7400aac89505d285
chanmaobaobao <2462338129@qq.com>
2017-10-17 09:50:52 +0800

    second commit

--- stdBP.py
+++ stdBP.py
@@ -1,20 +1,9 @@
 # coding: utf-8
+# 标准的BP 我们没运行一条数据改变更新一次参数,在一次数据集的遍历只把误差累计起来，各参数的导数只用每次求得的导数更新，不用累计！
 import pandas as pd
 from pandas import *
 import numpy as np
 from numpy import *
-# 取数据，转换数据形式
-# df = pd.read_csv(r"C:\Users\zmy\Desktop\titanic\watermelon.csv",skiprows=[2],header=None)
-#
-# df = df.transpose()
-# state = ['density','rate','result']
-# df.rename(columns={0:'density',1:'rate',2:'result'},inplace = True)
-# # print df
-# x = df[['density','rate']]
-# x = array(x)
-# result = df['result']
-# result = array(result)
-
 x = pd.read_csv(r"C:\Users\zmy\Desktop\titanic\xigua4.csv",header=None,nrows=8)
 x = x.transpose()
 x = array(x)
@@ -22,12 +11,9 @@ result = pd.read_csv(r"C:\Users\zmy\Desktop\titanic\xigua4.csv",header=None,skip
 result = result.transpose()
 result = array(result)
 result = result - 1
-print x
-print result
 
 #bp算法
 m,n = shape(x)
-print m,n
 t = 1
 v = np.random.rand(n,n+1) # 输入层与隐含层的权值
 w = np.random.rand(n+1, t)# 隐含层和输出层的权值
@@ -35,17 +21,17 @@ thy = np.random.rand(n+1)# 隐含层的阈值
 tho = np.random.rand(t)# 输出层的阈值
 out = np.zeros((m,t))# 输出层的输出值
 bn = np.zeros(n+1)#隐含层的输出值
-gj = np.zeros(1)
+gj = np.zeros(t)
 eh = np.zeros(n+1)
-xk = 0.1
+xk = 1
 
 kn = 0 # 迭代次数
 sn = 0 #
 old_ey = 0
-
+ii = 5
 while(1):
+    ii -= 1
     kn = kn + 1
-    # print 'kn',kn
     ey = 0
     for i in range(0,m):
         #计算隐含层输出
@@ -66,12 +52,12 @@ while(1):
             # print 'ey', ey
         # 计算gj
         for h1 in range(0,t):
-            gj[h1] = out[i][h1]*(1-out[i][h1])*(result[i] - out[i][h1])
+            gj[h1] =  out[i][h1]*(1-out[i][h1])*(result[i] - out[i][h1])
             # print out[i][h1],result[i]
         # 计算eh
         for h1 in range(0,n+1):
             for h2 in range(0, t):
-                eh[h1] = eh[h1] + bn[h1] * (1 - bn[h1]) * w[h1][h2]*gj[h2]
+                eh[h1] = bn[h1] * (1 - bn[h1]) * w[h1][h2]*gj[h2]
         # 更新w
         for h2 in range(0, t):
             for h1 in range(0,n+1):
@@ -82,7 +68,7 @@ while(1):
         # 更新输入层与隐含层的权值
         for h2 in range(0, n + 1):
             for h1 in range(0, n):
-                v[h1][h2] = v[h1][h2] + h1 * eh[h2] * x[i][h1]
+                v[h1][h2] = v[h1][h2] + xk * eh[h2] * x[i][h1]
         #更新隐含层阈值
         for h1 in range(0,n+1):
             thy[h1] = thy[h1] - xk * eh[h1]
